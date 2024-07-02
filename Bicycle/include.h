901bbc584ccd707476138289d49cc812f53568ad
jmartinezar <jmartinezar@unal.edu.co>
2024-07-02 12:55:45 -0500

    had added comments about units in constant

--- Bicycle/include.h
+++ Bicycle/include.h
@@ -1,11 +1,11 @@
 #pragma once
 
-const double v_0 = 1.0;
-const double P = 1.0;
-const double mass = 1.0;
-const double tinit = 0.0;
-const double tfinal = 10.0;
+const double v_0 = 1.0; //[m/s]
+const double P = 1.0; //[W]
+const double mass = 1.0; //[kg]
+const double tinit = 0.0; //[s]
+const double tfinal = 10.0; //[s]
 const int Nsteps = 1000;
-const double dt = (tfinal - tinit)/Nsteps;
+const double dt = (tfinal - tinit)/Nsteps; //[s]
 
 double velocity(double vi);
